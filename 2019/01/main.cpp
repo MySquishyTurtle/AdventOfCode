16bcec7f2f6ac4c8328edba257d5aa6e9e3db9f2
kendricksellers <contact@kendricksellers.me>
2019-12-01 13:44:08 -0500

    initial commit

---
+++ 2019/01/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::string;
+using std::vector;
+using std::stoi;
+
+int getFuel(int mass);
+
+int main() {
+    ifstream file("input.txt");
+    int fuelReqs = 0;
+    int mass;
+
+    while (file >> mass) {
+        fuelReqs += getFuel(mass);
+    }
+
+    cout << fuelReqs << endl;
+    return 0;
+}
+
+int getFuel(int mass) {
+    int fuel = 0;
+    fuel += mass / 3 - 2;
+    if (fuel < 0) {
+        return 0;
+    }
+    fuel += getFuel(fuel);
+    return fuel;
+}
